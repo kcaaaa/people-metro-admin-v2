c84b6519cc51db8b8745682cd37a81caffb1fea5
kcaaaa <2473994475@qq.com>
2025-07-05 17:04:59 +0800

    fix: 同步 vendor 目录到 docs

--- sync-to-docs.bat
+++ sync-to-docs.bat
@@ -1,7 +1,17 @@
 @echo off
 echo Syncing files to docs directory...
 
+rem Create necessary directories if they don't exist
+if not exist "docs\vendor" mkdir "docs\vendor"
+if not exist "docs\js" mkdir "docs\js"
+
+rem Sync JavaScript files
 xcopy /y /s js\* docs\js\
+
+rem Sync vendor files
+xcopy /y /s vendor\* docs\vendor\
+
+rem Sync index.html
 xcopy /y index.html docs\
 
 echo Sync completed!
