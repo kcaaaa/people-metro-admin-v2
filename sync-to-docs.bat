78c5ddce593b8847f44e056aba91ba18b04143d8
kcaaaa <2473994475@qq.com>
2025-07-06 23:03:16 +0800

    fix: restore docs directory with required files

--- sync-to-docs.bat
+++ sync-to-docs.bat
@@ -1,18 +1,26 @@
 @echo off
-echo Syncing files to docs directory...
+echo 正在同步文件到docs目录...
 
-rem Create necessary directories if they don't exist
-if not exist "docs\vendor" mkdir "docs\vendor"
-if not exist "docs\js" mkdir "docs\js"
+rem 创建docs目录（如果不存在）
+if not exist "docs" mkdir docs
 
-rem Sync JavaScript files
-xcopy /y /s js\* docs\js\
+rem 复制必要文件
+copy /Y index.html docs\
+copy /Y .nojekyll docs\
 
-rem Sync vendor files
-xcopy /y /s vendor\* docs\vendor\
+rem 复制js目录
+if exist "js" (
+    xcopy /Y /E /I js docs\js
+)
 
-rem Sync index.html
-xcopy /y index.html docs\
+rem 复制vendor目录
+if exist "vendor" (
+    xcopy /Y /E /I vendor docs\vendor
+)
 
-echo Sync completed!
-pause 
+rem 复制assets目录
+if exist "assets" (
+    xcopy /Y /E /I assets docs\assets
+)
+
+echo 同步完成！ 
